c7e0befcb2623f2b3535fce03238df38fcb7f805
susy-bakaa <aksu.kotikone@gmail.com>
2025-09-04 15:46:41 +0300

    bump version

--- raidsim/Assets/Scripts/Core/GlobalVariables.cs
+++ raidsim/Assets/Scripts/Core/GlobalVariables.cs
@@ -8,13 +8,13 @@ namespace dev.susybaka.raidsim.Core
     {
         // This value is incremented when the game gets an update.
         // It is used by WebGL to determine if the game assets need to be redownloaded and by the auto updater for version checks for updates.
-        public static int versionNumber = 8;
+        public static int versionNumber = 9;
         // Increment this when the scripts are changed in a way that will require a rebuild of all scenes inside bundles.
-        // (This is not in used yet but added for future reference)
+        // (This is not in use yet but added for future reference)
         public static int scriptingVersion = 1;
 #if UNITY_EDITOR
         public static string configPath = Application.dataPath + "/config.ini";
-        public static string bgmPath = "F:/Users/Aki/Files/GitHub/ffxiv-raid-sim/raidsim/Source/Audio/bgm";
+        public static string bgmPath = "F:/Git/ffxiv-raid-sim/raidsim/Source/Audio/bgm";
 #elif PLATFORM_STANDALONE_WIN
         public static string configPath = Path.GetDirectoryName(Application.dataPath) + "/config.ini";
         public static string bgmPath = Path.GetDirectoryName(Application.dataPath) + "/bgm";
