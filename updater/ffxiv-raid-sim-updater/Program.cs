5cd6f74e772a734c1d4b9de0445968c7d53b8df1
susy-bakaa <aksu.kotikone@gmail.com>
2025-09-22 05:48:42 +0300

    Add SPDX to updater

--- updater/ffxiv-raid-sim-updater/Program.cs
+++ updater/ffxiv-raid-sim-updater/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// SPDX-License-Identifier: GPL-3.0-only
+// This file is part of ffxiv-raid-sim. Linking with the Unity runtime
+// is permitted under the Unity Runtime Linking Exception (see LICENSE).
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
